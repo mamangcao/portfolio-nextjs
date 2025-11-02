427a7b6ecf8914d67024d432a26dba01a05f1939
Abu Saad As-Siddique <abdulhaleem.mamangcao@gmail.com>
2025-11-02 10:44:22 +0800

    Change icons

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,11 @@
-import { User, Mail, Info, Blocks, Briefcase, GraduationCap, Folder, FileBadge, Computer, ArrowUpRight, IdCard } from "lucide-react";
+import { User, Mail, Info, Blocks, GraduationCap, FileBadge, Computer, ArrowUpRight, IdCard, BriefcaseBusiness, FolderCode } from "lucide-react";
 
 export default function Card({ title, children, viewAllLink }) {
 
   const icons = {
     "About Me": <User className="w-4 h-4 text-black dark:text-white" />,
-    "Projects": <Folder className="w-4 h-4 text-black dark:text-white" />,
-    "Experience": <Briefcase className="w-4 h-4 text-black dark:text-white" />,
+    "Projects": <FolderCode className="w-4.5 h-4.5 text-black dark:text-white" />,
+    "Experience": <BriefcaseBusiness className="w-4 h-4 text-black dark:text-white" />,
     "Contact": <Mail className="w-4 h-4 text-black dark:text-white" />,
     "Education": <GraduationCap className="w-4.5 h-4.5 text-black dark:text-white" />,
     "Tech Stack": <Blocks className="w-4 h-4 text-black dark:text-white" />,
