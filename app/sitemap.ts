9aba5d679187d090660886cd991bf6d5217b8a01
Abu Saad As-Siddique <abdulhaleem.mamangcao@gmail.com>
2025-11-25 21:41:03 +0800

    Sitemap.ts and Robots.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-export default function sitemap() {
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://mamangcao.vercel.app/',
